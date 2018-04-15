c3bbd79f3b96b8e032dd06c4863274f06521430e
Dommert <dommertenterprises@gmail.com>
2018-04-14 22:41:42 -0700

    setup.py

---
+++ setup.py
@@ -0,0 +1,12 @@
+# Setup File
+
+from setuptools import setup
+
+setup(
+    name='yourapplication',
+    packages=['yourapplication'],
+    include_package_data=True,
+    install_requires=[
+        'flask',
+    ],
+)
