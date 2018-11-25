ae0d6540a1330260f5599cee73be24aa0579c8de
dommert <dommert@gmail.com>
2018-11-25 17:01:35 -0600

    updateNote

--- flask_netpad/netpad.py
+++ flask_netpad/netpad.py
@@ -42,6 +42,20 @@ def newNote(slug, content, title=None, **kwargs)
         return errorCode(404, 'Note not Created!')
 
 # Update Note
+def updateNote(nid, noteData):
+    try:
+        note=Note.objects(id=nid).get()
+        note.content= noteData.content
+        note.title= noteData.title
+        note.slug= noteData.slug
+        note.save()
+        return note
+    except:
+        return errorCode(404, 'Note not Found!')
+
 
 # Delete / Remove Note
+def delNote(nid):
+    # Soft Delete note
+    return 'deleted'
 
