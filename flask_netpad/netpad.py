6893c727d9c0d870a206cff1efccb4c7515aa375
dommert <dommert@gmail.com>
2018-11-25 16:41:19 -0600

    update to netpad

--- flask_netpad/netpad.py
+++ flask_netpad/netpad.py
@@ -4,4 +4,24 @@
 ## netpad.py [MongoDB logic]
 
 
+from flask_netpad.models import db, Note
+
+
+# Custom Error
+def errorCode(code=404, msg='Object Not Found :( '):
+    error = dict()
+    error['code'] = code
+    error['error'] = msg
+    return error
+
+
+# List Notes
+
+# Read Note (id)
+
+# New / Create Note
+
+# Update Note
+
+# Delete / Remove Note
 
