74c4d59ab6642c9236abe9d0a50fdbf2628d2ee6
dommert <dommert@gmail.com>
2018-11-28 00:40:57 -0600

    inline comment

--- flask_netpad/netpad.py
+++ flask_netpad/netpad.py
@@ -8,29 +8,40 @@ from flask_netpad.models import db, Note
 
 
 # Custom Error
+
 def errorCode(code=404, msg='Object Not Found :( '):
+    """
+    Returns a custom error code in a dictionary
+    :param code: Error Code 
+    :param msg: Message to return
+    :return: error
+    """
     error = dict()
     error['code'] = code
     error['error'] = msg
     return error
 
-def creatDB():
-    return 'DB Created! (hopefully)'
-
+def createDB(*args, **kwargs):
+    try:
+        note = Note(*args,**kwargs)
+        note.STATUS = "Note Created"
+        return note
+    except:
+        errorCode()
 
 # List Notes
-def listNote():
+def listNote(page=0, batch=40):
     try:
-        note = Note.objects.all()
+        note = Note.objects.paginate(page=page, per_page=batch)
         return note
     except:
         return errorCode()
 
 
 # Read Note (id)
-def readNote(nid):
+def readNote(*args, **kwargs):
     try:
-        note = Note.objects(id=nid)
+        note = Note.objects(*args, **kwargs)
         print(note.count())
         return note
     except:
@@ -47,6 +58,7 @@ def newNote(slug, content, title=None, **kwargs):
 # Update Note
 def updateNote(nid, noteData):
     try:
+        # BlogPost.objects(id=post.id).update(title='Example Post')
         note=Note.objects(id=nid).get()
         note.content= noteData.content
         note.title= noteData.title
@@ -60,5 +72,9 @@ def updateNote(nid, noteData):
 # Delete / Remove Note
 def delNote(nid):
     # Soft Delete note
+    try:
+        note = Note.objects(_id=nid).first()
+    except:
+        return errorCode()
     return 'deleted'
 
