bce04d84868c7acf69d0a4902984ee548b529e5b
Dommert <JDommert@cisco.com>
2020-07-15 01:02:01 -0700

    Update todo.py

--- flask_postaroma/router/todo.py
+++ flask_postaroma/router/todo.py
@@ -3,37 +3,30 @@ from flask_restful import Resource
 
 
 class Todo(Resource):
-    def get(self, todo_id=False):
+    def get(self, todo_id=None):
         return jsonify(test="12345", id=todo_id)
 
 
 ##  To Do CRUD
 
 # Create
-class Create(Resource):
-    def get(self, todo_id=False):
-        return jsonify(test="12345", id=todo_id)
+class Crud(Resource):
+    def get(self, todo_id=None):
+        return jsonify(method='get', test="12345", id=todo_id)
+
+    def post(self, todo_id=None):
+        return jsonify(method='post', test="12345", id=todo_id)
+
+    def update(self, todo_id=None):
+        return jsonify(method='update', test="12345", id=todo_id)
+
+    def delete(self, todo_id=None):
+        return jsonify(method='delete', test="12345", id=todo_id)
+
+    def patch(self, todo_id=None):
+        return jsonify(method='patch', test="12345", id=todo_id)
 
 
-# Read
-class Read(Resource):
-    def get(self, todo_id=False):
-        return jsonify(test="12345", id=todo_id)
-
-
-# Update
-class Update(Resource):
-    def get(self, todo_id=False):
-        return jsonify(test="12345", id=todo_id)
-
-
-# Delete
-class Delete(Resource):
-    def get(self, todo_id=False):
-        return jsonify(test="12345", id=todo_id)
-
-
-# List
 class List(Resource):
-    def get(self, todo_id=False):
-        return jsonify(test="12345", id=todo_id)
+    def get(self, todo_id=None):
+        return jsonify(method= "get", test="list dir", id=todo_id)
