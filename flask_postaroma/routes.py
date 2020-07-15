17bbb21c6ce076daafc75037f33cf675564beb91
Dommert <JDommert@cisco.com>
2020-07-15 01:02:05 -0700

    Update routes.py

--- flask_postaroma/routes.py
+++ flask_postaroma/routes.py
@@ -1,6 +1,8 @@
 from flask_postaroma.app import api
 from flask_postaroma.router import todo
 
-# api.add_resource(TodoList, '/todos')
 api.add_resource(todo.Todo, '/test')
-api.add_resource(todo.Read, '/todos/', '/todos/<todo_id>')
+
+# To Do
+api.add_resource(todo.List, '/todos/')
+api.add_resource(todo.Crud,  '/todo/<todo_id>')
